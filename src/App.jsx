53e64c3763ebcd427def847bd240db1a8cbd602d
georgetsai2028 <115137756+georgetsai2028@users.noreply.github.com>
2025-06-09 22:02:44 -0700

    added about me

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,18 @@ function App() {
     <>
     <div className='mainContainer'>
       <div className='description'>
+        <h1>Hey my name is George!</h1>
+        <QuoteDisplay></QuoteDisplay>
         <div className='photo'></div>
       </div>
       <NavBar></NavBar>
       <div id='about'>
-      <QuoteDisplay></QuoteDisplay>
+        <h1>About Me</h1>
+        <p>Second Year CS Major at Chaffey College with an interest in Web Development and Embedded Systems.
+          My programming journey begin in 2023, and since then I have created various of projects in many different technologies. 
+          I love playing basketball, rock climbing and going to the gym. Some other hobbies I love doing are making videos, playing piano, and learning languages
+        </p>
+        <p>I love making cool stuff! </p>
       </div>
       <div id='project'>
 
